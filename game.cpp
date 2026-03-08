6c4b3cd88b4722615801f93963d9ca3b44ce9f3c
Cattix-rs <ruben.stolwijk@gmail.com>
2026-03-08 13:10:02 +0100

    changelog
    bottem colisin now works think cause of jeramiah and changed bg

--- game.cpp
+++ game.cpp
@@ -43,35 +43,43 @@ namespace Tmpl8
 
 	namespace
 	{
-		Surface tiles("assets/Starry_Night_tiles_and_sprites.png");
+		Surface tiles("assets/Pixelart-drawing_14.png");
 	}
 	
 	
 
 
-	char map[10][48] = {
-		"aa ba ca da ea fa ga ha aa ba ca da ea fa ga ha",
-		"ab bb cb db eb fb gb hb ab bb cb db eb fb gb hb",
-		"ac bc cc dc ec fc gc hc ac bc cc dc ec fc gc hc",
-		"ad bd cd dd ed fd gd hd ad bd cd dd ed fd gd hd",
-		"ae be ce de ee fe ge he ae be ce de ee fe ge he",
-		"aa ba ca da ea fa ga ha aa ba ca da ea fa ga ha",
-		"ab bb cb db eb fb gb hb ab bb cb db eb fb gb hb",
-		"ac bc cc dc ec fc gc hc ac bc cc dc ec fc gc hc",
-		"ad bd cd dd ed fd gd hd ad bd cd dd ed fd gd hd",
-		"ae be ce de ee fe ge he ae be ce de ee fe ge he",
+	char map[16][75] = {
+		"cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb ad bd cd ad bd cd ad bd cd ad bd cd ad bd cd bd cd ad bd ad bd cd ad cb",
+		"cb cb bb ca ab bb ca ab bb ca ab bb ca ab bb ca ab bb ca ab bb ca bb cb cb",
+		"bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc bc"
+
+		
 	};
 	void DrawTile(int tx, int ty, Surface* screen, int x, int y)
 	{
-		Pixel* src = tiles.GetBuffer() + tx * 32 + (ty * 32) * 256;
+		Pixel* src = tiles.GetBuffer() + tx * 32 + (ty * 32) * 96;
 		Pixel* dst = screen->GetBuffer() + x + y * 800;
 		for (int i = 0; i < 32; i++)
 			for (int j = 0; j < 32; j++)
 			{
 				// Check alpha threshold.
-				unsigned char a = (src[j + i * 256] >> 24) & 0xFF;
+				unsigned char a = (src[j + i * 96] >> 24) & 0xFF;
 				if (a > 128)
-					dst[j + i * 800] = src[j + i * 256];
+					dst[j + i * 800] = src[j + i * 96];
 
 			}
 	}
@@ -93,8 +101,8 @@ namespace Tmpl8
 		screen->Clear(0);
 
 		player.Update(deltaTime);
-		for (int y = 0; y < 10; y++)
-			for (int x = 0; x < 16; x++)
+		for (int y = 0; y < 16; y++)
+			for (int x = 0; x < 25; x++)
 			{
 				int tx = map[y][x * 3] - 'a';
 				int ty = map[y][x * 3 + 1] - 'a';
