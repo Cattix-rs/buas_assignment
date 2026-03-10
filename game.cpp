c18d8ec3c8b0ea3af163ae510065022a2b8f4a5e
Cattix-rs <ruben.stolwijk@gmail.com>
2026-03-10 16:42:51 +0100

    small cngane to cometed code

--- game.cpp
+++ game.cpp
@@ -86,13 +86,7 @@ namespace Tmpl8
 	// -----------------------------------------------------------
 	// Main application tick function
 	// -----------------------------------------------------------
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
-	//Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
+
 	
 	//int i;
 	
@@ -115,10 +109,6 @@ namespace Tmpl8
 		screen->Box(player.getAABB(), 0xffffffff);
 	}
 };/// making sure it renders and compiles
-//gaffer on games 
-//sprisheet 
-//phyiscs 
-// met manon mail sturen naar buas for huidige education verification
-// sprite animation base class all frames and all using vectors 
-//aabb assing to player and edge of scr4een 
+
+
 //tiled https://discord.com/channels/515453022097244160/1407719742541922374/1467979701610877102 https://discord.com/channels/515453022097244160/1407719742541922374/1467979756602527921
