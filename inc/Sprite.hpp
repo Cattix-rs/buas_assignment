f85914aec9bb3cc2a3e878075ea7caf8715347f7
Cattix-rs <ruben.stolwijk@gmail.com>
2026-03-29 11:37:57 +0200

    working on Own sprite class and added new namespace

---
+++ inc/Sprite.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <filesystem>
+#include <memory>
+
+#include "../surface.h"
+#include "physics.hpp"
+
+namespace Atlas
+{
+	class Sprite
+	{
+	public:
+		Sprite() = default;
+		explicit Sprite(const std::filesystem::path& fileName, const Tmpl8::RectI& rect, const BlendMode& blendMode = BlendMode{});
+		explicit Sprite(std::shared_ptr<Image> image, const Tmpl8::RectI& rect, const BlendMode& blendMode = BlendMode {}) noexcept; // 
+		explicit Sprite(const std::filesystem::path& fileName, const BlendMode& blendMode = BlendMode{});
+		explicit Sprite(std::shared_ptr<Image> image, const BlendMode& blendMode = BlendMode{}) noexcept; // 
+
+		const Tmpl8::RectI& GetRect() const noexcept
+		{
+			return m_Rect;
+		}
+		std::shared_ptr<Image> GetImage() const noexcept
+		{
+			return m_Image;
+		}
+		const Color& GetColor() const noexcept
+		{
+			return m_Color;
+		}
+		const BlendMode& GetBlendMode() const noexcept
+		{
+			return m_BlendMode;
+		}
+	private:
+		std::shared_ptr<Image> m_Image;
+		Tmpl8::RectI m_Rect;
+		BlendMode m_BlendMode;
+	};
+}
