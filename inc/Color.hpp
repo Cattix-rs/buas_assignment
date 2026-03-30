c56ef899ba3dc7f79d65078e15e9add9e835c03d
Cattix-rs <ruben.stolwijk@gmail.com>
2026-03-30 18:00:42 +0200

    worked on Color/hpp

--- inc/Color.hpp
+++ inc/Color.hpp
@@ -9,6 +9,18 @@ namespace Atlas
 
 		constexpr explicit Color(uint32_t rgba) noexcept;
 
+		constexpr Color(uint8_t r, uint8_t g, int8_t b, uint8_t a = 255u)noexcept;
 
+		~Color() noexcept = default;
+		constexpr Color(const Color&) noexcept = default;
+		constexpr Color(Color&&) noexcept = default;
+		constexpr Color& operator=(const Color&) = default;
+		constexpr Color& operator=(Color&&) = default;
+
+		constexpr auto operator<=>(const Color& rhs) const noexcept;
+		constexpr bool operator==(const Color& rhs) const noexcept;
+
+		Color operator+(const Color& rhs) const noexcept;
+		Color& operator+=(const Color& rhs) noexcept;
 	};
 }
