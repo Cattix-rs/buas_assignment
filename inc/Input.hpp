b33124c897be7c7fe8a15e7a53251274973ce886
Cattix-rs <ruben.stolwijk@gmail.com>
2026-04-01 21:10:09 +0200

    Input fully defined

--- inc/Input.hpp
+++ inc/Input.hpp
@@ -32,7 +32,33 @@ namespace Tmpl8
         static bool Enter() { return IsPressed(SDL_SCANCODE_RETURN); }
         static bool Space() { return IsPressed(SDL_SCANCODE_SPACE); }
         // for naming
-        static bool A()  { return IsPressed(SDL_SCANCODE_A); } 
+        static bool A() { return IsPressed(SDL_SCANCODE_A); } 
+        static bool B() { return IsPressed(SDL_SCANCODE_B); }
+        static bool C() { return IsPressed(SDL_SCANCODE_C); }
+        static bool D() { return IsPressed(SDL_SCANCODE_D); }
+        static bool E() { return IsPressed(SDL_SCANCODE_E); }
+        static bool F() { return IsPressed(SDL_SCANCODE_F); }
+        static bool G() { return IsPressed(SDL_SCANCODE_G); }
+        static bool H() { return IsPressed(SDL_SCANCODE_H); }
+        static bool I() { return IsPressed(SDL_SCANCODE_I); }
+        static bool J() { return IsPressed(SDL_SCANCODE_J); }
+        static bool K() { return IsPressed(SDL_SCANCODE_K); }
+        static bool L() { return IsPressed(SDL_SCANCODE_L); }
+        static bool M() { return IsPressed(SDL_SCANCODE_M); }
+        static bool N() { return IsPressed(SDL_SCANCODE_N); }
+        static bool O() { return IsPressed(SDL_SCANCODE_O); }
+        static bool P() { return IsPressed(SDL_SCANCODE_P); }
+        static bool Q() { return IsPressed(SDL_SCANCODE_Q); }
+        static bool R() { return IsPressed(SDL_SCANCODE_R); }
+        static bool S() { return IsPressed(SDL_SCANCODE_S); }
+        static bool T() { return IsPressed(SDL_SCANCODE_T); }
+        static bool U() { return IsPressed(SDL_SCANCODE_U); }
+        static bool V() { return IsPressed(SDL_SCANCODE_V); }
+        static bool W() { return IsPressed(SDL_SCANCODE_W); }
+        static bool X() { return IsPressed(SDL_SCANCODE_X); }
+        static bool Y() { return IsPressed(SDL_SCANCODE_Y); }
+        static bool Z() { return IsPressed(SDL_SCANCODE_Z); }
+      
 
 
         static bool AnyKeyDown() {
